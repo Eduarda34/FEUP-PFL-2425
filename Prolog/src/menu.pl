d00d6a3667976589a42af961adbcb1ae10f8542c
eduardaraujo <mpmaeduarda2002@gmail.com>
2024-12-04 19:58:50 +0000

    amo o gpt

--- Prolog/src/menu.pl
+++ Prolog/src/menu.pl
@@ -1,25 +1,25 @@
 main_menu :-
-    write('=== MENU DO JOGO ==='), nl,
+    write('=== MENU ==='), nl,
     write('1. Iniciar jogo'), nl,
     write('2. Ver instruções'), nl,
     write('3. Sair'), nl,
-    write('Escolha uma opção: '),
+    write('Escolhe uma opção: '),
     read(Option),
     handle_option(Option).
 
 handle_option(1) :-
-    write('Jogo iniciado!'), nl,
+    write('A entrar no jogo!'), nl,
     start_game.
 handle_option(2) :-
     write('=== INSTRUÇÕES ==='), nl,
-    write('1. Faça isso.'), nl,
-    write('2. Faça aquilo.'), nl,
+    write('1. X'), nl,
+    write('2. Y'), nl,
     main_menu.
 handle_option(3) :-
-    write('Saindo do jogo...'), nl.
+    write('A sair do jogo...'), nl.
 handle_option(_) :-
-    write('Opção inválida. Tente novamente.'), nl,
+    write('Opção inválida. Tenta novamente.'), nl,
     main_menu.
 
 start_game :-
-    write('Bem-vindo ao jogo!'), nl.
+    write('Bem-vindo ao Doblin!'), nl.

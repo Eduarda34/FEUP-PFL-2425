4354c47d8c19f4af0f67916c63586b3cad2e9581
Luís Contreiras <luis.m.contreiras@gmail.com>
2024-12-28 19:11:20 +0000

    Menu alterações

--- Prolog/src/menu.pl
+++ Prolog/src/menu.pl
@@ -1,5 +1,4 @@
-/* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; 
-   prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
+/* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
 
 main_menu :-
     write('=== MENU ==='), nl,

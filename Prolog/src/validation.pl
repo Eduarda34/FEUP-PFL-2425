1f8c7ea94fccf9ece8394f69a5a0a962daff2fdc
Luís Contreiras <luis.m.contreiras@gmail.com>
2024-12-28 19:09:40 +0000

    Initial commit

--- Prolog/src/validation.pl
+++ Prolog/src/validation.pl
@@ -1,3 +1,5 @@
+/* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
+
 % Validate a Move
 valid_move(Row, Col, Grid) :-
     nth1(Row, Grid, CurrentRow),
